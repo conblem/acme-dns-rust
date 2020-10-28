016366c989d7fc59e7a71ebc0d10349e31003f65
conblem <ramonwehrli@gmail.com>
2020-10-28 10:40:44 +0100

    fix: dns metrics

--- src/api/metrics.rs
+++ src/api/metrics.rs
@@ -1,8 +1,8 @@
 use lazy_static::lazy_static;
-use prometheus::{register_histogram_vec, Encoder, HistogramVec, TextEncoder, HistogramTimer};
+use prometheus::{register_histogram_vec, Encoder, HistogramTimer, HistogramVec, TextEncoder};
 use tracing::error;
-use warp::{http::Response, Filter, Rejection, Reply};
 use warp::filters::path::FullPath;
+use warp::{http::Response, Filter, Rejection, Reply};
 
 lazy_static! {
     static ref HTTP_REQ_HISTOGRAM: HistogramVec = register_histogram_vec!(
@@ -15,10 +15,12 @@ lazy_static! {
 
 pub(super) fn metrics_wrapper<R: Reply>(
     filter: impl Filter<Extract = (R,), Error = Rejection> + Clone + Send + 'static,
-) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone + Send + 'static {
+) -> impl Filter<Extract = (R,), Error = Rejection> + Clone + Send + 'static {
     warp::path::full()
         .map(|path: FullPath| {
-            HTTP_REQ_HISTOGRAM.with_label_values(&[path.as_str()]).start_timer()
+            HTTP_REQ_HISTOGRAM
+                .with_label_values(&[path.as_str()])
+                .start_timer()
         })
         .and(filter)
         .map(|timer: HistogramTimer, res: R| {
--- src/dns/handler.rs
+++ src/dns/handler.rs
@@ -1,9 +1,39 @@
+use futures_util::future::{Map, Join, Ready};
+use futures_util::FutureExt;
+use lazy_static::lazy_static;
+use prometheus::{register_histogram_vec, HistogramTimer, HistogramVec};
+use std::future::Future;
 use tracing::field::Empty;
 use tracing::instrument::Instrumented;
 use tracing::{info_span, Instrument, Span};
+use trust_dns_client::op::LowerQuery;
 use trust_dns_server::authority::Catalog;
 use trust_dns_server::server::Request;
 use trust_dns_server::server::{RequestHandler, ResponseHandler};
+
+lazy_static! {
+    static ref DNS_REQ_HISTOGRAM: HistogramVec = register_histogram_vec!(
+        "dns_request_duration_seconds",
+        "The DNS request latencies in seconds.",
+        &["name"]
+    )
+    .unwrap();
+}
+
+fn end_timer(input: (ResponseFutureOutput, HistogramTimer)) {
+    let (res, timer) = input;
+    timer.observe_duration();
+
+    res
+}
+
+type ResponseFuture = <Catalog as RequestHandler>::ResponseFuture;
+type ResponseFutureOutput = <ResponseFuture as Future>::Output;
+
+type MapFn = fn((ResponseFutureOutput, HistogramTimer));
+
+type JoinedFuture = Join<ResponseFuture, Ready<HistogramTimer>>;
+type MappedFuture = Map<Instrumented<JoinedFuture>, MapFn>;
 
 pub(super) struct TraceRequestHandler {
     catalog: Catalog,
@@ -17,16 +47,39 @@ impl TraceRequestHandler {
 }
 
 impl RequestHandler for TraceRequestHandler {
-    type ResponseFuture = Instrumented<<Catalog as RequestHandler>::ResponseFuture>;
+    type ResponseFuture = MappedFuture;
 
     fn handle_request<R: ResponseHandler>(
         &self,
         request: Request,
         response_handle: R,
     ) -> Self::ResponseFuture {
+        let name = request
+            .message
+            .queries()
+            .first()
+            .map(LowerQuery::name)
+            .map(ToString::to_string);
+
+        let name = match name {
+            Some(ref name) => Some([name.as_str()]),
+            None => None,
+        };
+
+        let name = match name {
+            Some(ref name) => &name[..],
+            None => &[],
+        };
+
         let addr = request.src;
-        self.catalog
-            .handle_request(request, response_handle)
-            .instrument(info_span!(parent: &self.span, "request", remote.addr = %addr, name = Empty, query_type = Empty))
+        let span = info_span!(parent: &self.span, "request", remote.addr = %addr, name = Empty, query_type = Empty);
+
+        let timer =
+            futures_util::future::ready(DNS_REQ_HISTOGRAM.with_label_values(name).start_timer());
+        let handle_request = self.catalog.handle_request(request, response_handle);
+
+        futures_util::future::join(handle_request, timer)
+            .instrument(span)
+            .map(end_timer)
     }
 }
