687a76c650ea753cb1b5d7e1d79c18a0676b166e
conblem <ramonwehrli@gmail.com>
2021-02-11 19:53:05 +0100

    fix: finish modularization of acceptor

--- src/api/tls.rs
+++ src/api/tls.rs
@@ -13,6 +13,31 @@ use tracing::{error, info};
 use crate::facade::{Cert, CertFacade};
 use crate::util::to_u64;
 
+pub fn wrap_higher<L, I, S, A, F>(
+    listener: L,
+    acceptor: A,
+) -> impl Stream<
+    Item = Result<
+        impl Future<Output = Result<impl AsyncRead + AsyncWrite + Send + Unpin + 'static>>,
+    >,
+> + Send
+where
+    L: Stream<Item = IoResult<I>> + Send + 'static,
+    I: Future<Output = IoResult<S>> + Send + 'static,
+    S: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+    A: Fn() -> F + Clone + Send + 'static,
+    F: Future<Output = Result<TlsAcceptor>>,
+{
+    listener
+        .err_into()
+        .zip(repeat(acceptor))
+        .map(|(conn, acceptor)| conn.map(|c| (c, acceptor)))
+        .map_ok(|(conn, acceptor)| async move {
+            let (conn, tls) = tokio::try_join!(conn.err_into(), acceptor())?;
+            Ok(tls.accept(conn).await?)
+        })
+}
+
 pub fn wrap<L, I, S, F>(
     listener: L,
     facade: F,
@@ -22,21 +47,13 @@ pub fn wrap<L, I, S, F>(
     >,
 > + Send
 where
-    L: Stream<Item = IoResult<I>> + Send,
-    I: Future<Output = IoResult<S>> + Send,
+    L: Stream<Item = IoResult<I>> + Send + 'static,
+    I: Future<Output = IoResult<S>> + Send + 'static,
     S: AsyncRead + AsyncWrite + Send + Unpin + 'static,
-    F: CertFacade + Send + Sync,
+    F: CertFacade + Send + Sync + 'static,
 {
-    let acceptor = Acceptor::new(facade);
-
-    listener
-        .err_into()
-        .zip(repeat(acceptor))
-        .map(|(conn, acceptor)| conn.map(|c| (c, acceptor)))
-        .map_ok(|(conn, acceptor)| async move {
-            let (conn, tls) = tokio::try_join!(conn.err_into(), acceptor.load_cert())?;
-            Ok(tls.accept(conn).await?)
-        })
+    let acceptor = acceptor(facade);
+    wrap_higher(listener, acceptor)
 }
 
 trait Func: Fn() -> <Self as Func>::Output {
@@ -50,76 +67,75 @@ where
     type Output = O;
 }
 
-fn _test<F: CertFacade>(_facade: F) -> impl Func<Output = impl Future<Output = ()>> + Clone {
-    let test = Arc::new("Hallo".to_owned());
+fn acceptor<F>(
+    facade: F,
+) -> impl Func<Output = impl Future<Output = Result<TlsAcceptor>>> + Clone + 'static
+where
+    F: CertFacade + 'static,
+{
+    let server_config = ServerConfig::new(NoClientAuth::new());
+
+    let config = RwLock::new((None, Arc::new(server_config)));
+    let wrapper = Arc::new((facade, config));
+
     move || {
-        let test_two = Arc::clone(&test);
+        let wrapper = Arc::clone(&wrapper);
         async move {
-            println!("{}", test_two);
+            let (facade, config) = &*wrapper;
+            load_cert(facade, config).await
         }
     }
 }
 
-struct Acceptor<F> {
-    facade: F,
-    config: RwLock<(Option<Cert>, Arc<ServerConfig>)>,
+async fn load_cert<F>(
+    facade: &F,
+    config: &RwLock<(Option<Cert>, Arc<ServerConfig>)>,
+) -> Result<TlsAcceptor>
+where
+    F: CertFacade + 'static,
+{
+    let new_cert = facade.first_cert().await;
+
+    let db_cert = match (new_cert, &*config.read()) {
+        (Ok(Some(new_cert)), (cert, _)) if Some(&new_cert) != cert.as_ref() => new_cert,
+        (_, (_, server_config)) => {
+            info!("Using existing TLS Config");
+            return Ok(TlsAcceptor::from(Arc::clone(server_config)));
+        }
+    };
+    info!(timestamp = to_u64(&db_cert.update), "Found new cert");
+
+    let server_config = match create_server_config(&db_cert) {
+        Ok(server_config) => server_config,
+        Err(e) => {
+            error!("{}", e);
+            let (_, server_config) = &*config.read();
+            return Ok(TlsAcceptor::from(Arc::clone(server_config)));
+        }
+    };
+
+    *config.write() = (Some(db_cert), Arc::clone(&server_config));
+    info!("Created new TLS config");
+    Ok(TlsAcceptor::from(server_config))
 }
 
-impl<F: CertFacade> Acceptor<F> {
-    fn new(facade: F) -> Arc<Self> {
-        let server_config = ServerConfig::new(NoClientAuth::new());
+fn create_server_config(db_cert: &Cert) -> Result<Arc<ServerConfig>> {
+    let (private, cert) = match (&db_cert.private, &db_cert.cert) {
+        (Some(private), Some(cert)) => (private, cert),
+        _ => return Err(anyhow!("Cert has no Cert or Private")),
+    };
 
-        Arc::new(Acceptor {
-            facade,
-            config: RwLock::new((None, Arc::new(server_config))),
-        })
-    }
+    let mut privates = pkcs8_private_keys(&mut private.as_bytes())
+        .map_err(|_| anyhow!("Private is invalid {:?}", private))?;
+    let private = privates
+        .pop()
+        .ok_or_else(|| anyhow!("Private Vec is empty {:?}", privates))?;
 
-    fn create_server_config(&self, db_cert: &Cert) -> Result<Arc<ServerConfig>> {
-        let (private, cert) = match (&db_cert.private, &db_cert.cert) {
-            (Some(private), Some(cert)) => (private, cert),
-            _ => return Err(anyhow!("Cert has no Cert or Private")),
-        };
+    let cert = certs(&mut cert.as_bytes()).map_err(|_| anyhow!("Cert is invalid {:?}", cert))?;
 
-        let mut privates = pkcs8_private_keys(&mut private.as_bytes())
-            .map_err(|_| anyhow!("Private is invalid {:?}", private))?;
-        let private = privates
-            .pop()
-            .ok_or_else(|| anyhow!("Private Vec is empty {:?}", privates))?;
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    config.set_single_cert(cert, private)?;
+    config.set_protocols(&["h2".into(), "http/1.1".into()]);
 
-        let cert =
-            certs(&mut cert.as_bytes()).map_err(|_| anyhow!("Cert is invalid {:?}", cert))?;
-
-        let mut config = ServerConfig::new(NoClientAuth::new());
-        config.set_single_cert(cert, private)?;
-        config.set_protocols(&["h2".into(), "http/1.1".into()]);
-
-        Ok(Arc::new(config))
-    }
-
-    async fn load_cert(&self) -> Result<TlsAcceptor> {
-        let new_cert = self.facade.first_cert().await;
-
-        let db_cert = match (new_cert, &*self.config.read()) {
-            (Ok(Some(new_cert)), (cert, _)) if Some(&new_cert) != cert.as_ref() => new_cert,
-            (_, (_, server_config)) => {
-                info!("Using existing TLS Config");
-                return Ok(TlsAcceptor::from(Arc::clone(server_config)));
-            }
-        };
-        info!(timestamp = to_u64(&db_cert.update), "Found new cert");
-
-        let server_config = match self.create_server_config(&db_cert) {
-            Ok(server_config) => server_config,
-            Err(e) => {
-                error!("{}", e);
-                let (_, server_config) = &*self.config.read();
-                return Ok(TlsAcceptor::from(Arc::clone(server_config)));
-            }
-        };
-
-        *self.config.write() = (Some(db_cert), Arc::clone(&server_config));
-        info!("Created new TLS config");
-        Ok(TlsAcceptor::from(server_config))
-    }
+    Ok(Arc::new(config))
 }
