cc34d8d7dd4f15b1c5ab59efd9488b95db26019c
conblem <mail@conblem.me>
2022-06-01 10:14:26 +0200

    fix: create action

--- acme-dns-rust/src/lib.rs
+++ acme-dns-rust/src/lib.rs
@@ -19,139 +19,140 @@ thread_local! {
     pub static CACHED: Cell<Option<Arc<CertifiedKey>>> = Cell::new(None);
 }
 
-trait ResolverCache: Send + Sync {
-    type Guard: DerefMut<Target = Option<Arc<CertifiedKey>>>;
-
-    fn ms_since_last_checked(&self) -> u128;
-    fn set_last_checked(&self);
-    fn key(&self) -> Self::Guard;
-}
-
-struct ResolverCacheImpl {
-    last_checked: &'static LocalKey<Cell<Instant>>,
-    cached: &'static LocalKey<Cell<Option<Arc<CertifiedKey>>>>,
-}
-
-impl ResolverCacheImpl {
-    fn new() -> Self {
-        Self {
-            last_checked: &LAST_CHECKED,
-            cached: &CACHED,
-        }
-    }
-}
-
-struct ResolverCacheGuard {
-    cached: &'static LocalKey<Cell<Option<Arc<CertifiedKey>>>>,
-    key: Option<Arc<CertifiedKey>>,
-    _not_send_and_sync: PhantomData<*const ()>,
-}
-
-impl Drop for ResolverCacheGuard {
-    fn drop(&mut self) {
-        if let Some(key) = self.key.take() {
-            self.cached
-                .with(move |tls_cached| tls_cached.set(Some(key)));
-        }
-    }
-}
-
-impl Deref for ResolverCacheGuard {
-    type Target = Option<Arc<CertifiedKey>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.key
-    }
-}
-
-impl DerefMut for ResolverCacheGuard {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.key
-    }
-}
-
-impl ResolverCache for ResolverCacheImpl {
-    type Guard = ResolverCacheGuard;
-
-    fn ms_since_last_checked(&self) -> u128 {
-        let last_checked = self.last_checked.with(Cell::get);
-        Instant::now().duration_since(last_checked).as_millis()
-    }
-
-    fn set_last_checked(&self) {
-        self.last_checked
-            .with(|tls_last_checked| tls_last_checked.set(Instant::now()));
-    }
-
-    fn key(&self) -> Self::Guard {
-        let key = self.cached.with(Cell::take);
-
-        ResolverCacheGuard {
-            cached: self.cached,
-            key,
-            _not_send_and_sync: PhantomData,
-        }
-    }
-}
-
 struct SharedCachingCertResolverController {
-    inner: Arc<RwLock<Arc<CertifiedKey>>>,
+    shared_key: Arc<RwLock<Arc<CertifiedKey>>>,
 }
 
 impl SharedCachingCertResolverController {
     fn new(initial: Arc<CertifiedKey>) -> (Self, Arc<dyn ResolvesServerCert>) {
-        let inner = Arc::new(RwLock::new(initial));
+        let shared_key = Arc::new(RwLock::new(initial));
         let this = Self {
-            inner: Arc::clone(&inner),
-        };
-        let resolver = SharedCachingCertResolver {
-            inner,
-            cache: ResolverCacheImpl {
-                last_checked: &LAST_CHECKED,
-                cached: &CACHED,
-            },
+            shared_key: Arc::clone(&shared_key),
         };
 
-        (this, Arc::new(resolver))
+        let shared_resolver = SharedCertResolver { shared_key };
+        let caching_resolver = CachingCertResolver {
+            inner: shared_resolver,
+            cached: &CACHED,
+            last_checked: &LAST_CHECKED,
+        };
+
+        (this, Arc::new(caching_resolver))
     }
 
     fn set_cert(&self, cert: Arc<CertifiedKey>) {
-        let mut inner = self.inner.write().unwrap();
-        *inner = cert;
+        let mut shared_key = self.shared_key.write().unwrap();
+        *shared_key = cert;
     }
 }
 
-struct SharedCachingCertResolver<T> {
-    inner: Arc<RwLock<Arc<CertifiedKey>>>,
-    cache: T,
+struct SharedCertResolver {
+    shared_key: Arc<RwLock<Arc<CertifiedKey>>>,
 }
 
-impl<T: ResolverCache> ResolvesServerCert for SharedCachingCertResolver<T> {
+impl ResolvesServerCert for SharedCertResolver {
     fn resolve(&self, _client_hello: ClientHello) -> Option<Arc<CertifiedKey>> {
-        let mut cached_key = self.cache.key();
-        let cached_key = cached_key.deref_mut();
-
-        if self.cache.ms_since_last_checked() < CACHING_TIME_MS {
-            if let Some(cached_key) = cached_key {
-                return Some(Arc::clone(cached_key));
-            }
-        }
-
-        let guard = self.inner.read().unwrap();
-
-        self.cache.set_last_checked();
-        // check if we already have the newest key in the cache
-        match cached_key {
-            Some(cached_key) if Arc::ptr_eq(cached_key, &*guard) => {
-                return Some(Arc::clone(cached_key))
-            }
-            _ => {}
-        }
-
-        *cached_key = Some(Arc::clone(&*guard));
+        let guard = self.shared_key.read().unwrap();
         Some(Arc::clone(&*guard))
     }
 }
 
+struct CachingCertResolver<T> {
+    inner: T,
+    cached: &'static LocalKey<Cell<Option<Arc<CertifiedKey>>>>,
+    last_checked: &'static LocalKey<Cell<Instant>>,
+}
+
+impl<T: ResolvesServerCert> ResolvesServerCert for CachingCertResolver<T> {
+    fn resolve(&self, client_hello: ClientHello) -> Option<Arc<CertifiedKey>> {
+        let since_last_checked = self.last_checked.with(Cell::get).elapsed().as_millis();
+        if since_last_checked > 10 {
+            let cached_key = self.cached.with(Cell::take);
+            if let Some(cached_key) = cached_key {
+                self.cached.with({
+                    let cached_key = Some(Arc::clone(&cached_key));
+                    move |tls_cached_key| tls_cached_key.set(cached_key)
+                });
+                return Some(cached_key);
+            }
+        }
+
+        self.last_checked
+            .with(|tls_last_chekced| tls_last_chekced.set(Instant::now()));
+
+        let key = self.inner.resolve(client_hello);
+        self.cached.with({
+            let key = key.as_ref().map(Arc::clone);
+            move |tls_cached_key| tls_cached_key.set(key)
+        });
+        key
+    }
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::CachingCertResolver;
+    use rstest::*;
+    use rustls_pemfile::Item;
+    use std::cell::Cell;
+    use std::sync::Arc;
+    use std::time::{Duration, Instant};
+    use tokio_rustls::rustls::server::ResolvesServerCert;
+    use tokio_rustls::rustls::sign::CertifiedKey;
+    use tokio_rustls::rustls::{
+        Certificate, ClientConfig, ClientConnection, PrivateKey, RootCertStore, ServerConfig,
+    };
+
+    #[fixture]
+    fn certificate() -> Certificate {
+        let mut cert = include_bytes!("../tests/leaf.crt").as_slice();
+        let cert = rustls_pemfile::read_one(&mut cert).unwrap().unwrap();
+        match cert {
+            Item::X509Certificate(cert) => Certificate(cert),
+            _ => panic!("expected certificate"),
+        }
+    }
+
+    #[fixture]
+    fn private_key() -> PrivateKey {
+        let mut key = include_bytes!("../tests/leaf.key").as_slice();
+        let key = rustls_pemfile::read_one(&mut key).unwrap().unwrap();
+        match key {
+            Item::ECKey(key) => PrivateKey(key),
+            _ => panic!("expected certificate"),
+        }
+    }
+
+    #[fixture]
+    fn static_resolver(
+        certificate: Certificate,
+        private_key: PrivateKey,
+    ) -> Arc<dyn ResolvesServerCert> {
+        let config = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(vec![certificate], private_key)
+            .unwrap();
+
+        config.cert_resolver
+    }
+
+    #[rstest]
+    fn test_last_checked_cache(static_resolver: Arc<dyn ResolvesServerCert>) {
+        thread_local! {
+            static CACHED: Cell<Option<Arc<CertifiedKey>>> = Cell::new(None);
+            static LAST_CHECKED: Cell<Instant> = Cell::new(Instant::now());
+        }
+
+        let _resolver = CachingCertResolver {
+            cached: &CACHED,
+            last_checked: &LAST_CHECKED,
+            inner: static_resolver,
+        };
+
+        let _client_config = ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(RootCertStore::empty())
+            .with_no_client_auth();
+    }
+}
